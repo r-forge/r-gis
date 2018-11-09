45d552202e12f9ed154c6479cca501e4720228df
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2018-11-09 00:32:49 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gis@422 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/terra/R/zzz.R
+++ pkg/terra/R/zzz.R
@@ -1,10 +1,11 @@
 
 loadModule("spat", TRUE)
 
-.startupMessage <- function() {
+
+.onAttach <- function(libname, pkgname) {
 	minv <- "2.8.7"
 	m <- "\n\nThis is an early version of the \"terra\" package, for evaluation only\n"
-	e <- try(vv <- packageVersion("raster"), silent=T )
+	e <- try(vv <- utils::packageVersion("raster"), silent=T )
 	if (class(e)[1] == "try-error") {
 		m <- paste0(m,"For this package to work you need to install version ", minv, 
 		" or higher of the \"raster\" package\n" )	
@@ -12,11 +13,7 @@ loadModule("spat", TRUE)
 		m <- paste0(m,"For this package to work you need to install version ", minv, 
 		" or higher of the \"raster\" package\nYou have version: ", vv,"\n" )
 	} 
-	return(m)
-} 
-
-.onAttach <- function(libname, pkgname) {
-	packageStartupMessage( cat(.startupMessage()))
+	packageStartupMessage(m)
 }
 
 
