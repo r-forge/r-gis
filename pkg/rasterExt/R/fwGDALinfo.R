33a29eb90f0b54bd971521b755ce29cbf8f6dd6f
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2020-06-04 00:24:17 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gis@437 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rasterExt/R/fwGDALinfo.R
+++
@@ -1,9 +0,0 @@
-
-fwInfo <- function(x, options=NULL) {
-	fwp <- paste(fwPath(), 'gdalinfo', sep='')
-	x <- .getFilename(x)
-	outfile <- extension(rasterTmpFile(), '.txt')
-	fullcall <- paste(fwp, options, shQuote(x))
-	system(fullcall, intern=TRUE)
-}
-
